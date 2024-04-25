9a3d89d4b91b46d53304c0b7251133d54aba0efb
Petr Martínek <martip16@fit.cvut.cz>
2024-04-26 00:25:51 +0200

    hwBIG: add CellPosition

--- hwBIG/include/CellPosition.hpp
+++ hwBIG/include/CellPosition.hpp
@@ -2,13 +2,20 @@
 #define CELL_POSITION_INCLUDED
 
 #include <string_view>
+#include <string>
+#include <iostream>
 
 struct CellPosition
 {
-    CellPosition(std::string_view str);
+    explicit CellPosition(const std::string_view& str);
     
+    bool operator<(const CellPosition& other) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const CellPosition& pos);
+
 private:
-    // todo
+    size_t rowIndex;
+    std::string columnIndex; // maybe not a good idea? consider numbers if issues arise
 };
 
 using CPos = CellPosition; // for progtest
--- hwBIG/src/CellPosition.cpp
+++ hwBIG/src/CellPosition.cpp
@@ -1,4 +1,54 @@
 #include "../include/CellPosition.hpp"
 
 #include <string_view>
-// todo
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <cctype>
+#include <stdexcept>
+#include <algorithm>
+
+CellPosition::CellPosition(const std::string_view& str)
+{
+    if(str.size() < 2 || !std::isalpha(str.front()) || !std::isdigit(str.back()))
+    {
+        throw std::invalid_argument("Invalid cell identifier");
+    }
+
+    size_t position = 0;
+
+    while(std::isalpha(str[position]))
+    {
+        ++position;
+    }
+
+    columnIndex = str.substr(0, position);
+
+    std::istringstream iss(std::string{str.substr(position)});
+
+    if(!(iss >> rowIndex))
+    {
+        throw std::invalid_argument("Invalid row index");
+    }
+}
+
+//------------------------------------------------------------------------------
+
+bool CellPosition::operator<(const CellPosition& other) const
+{
+    if(rowIndex == other.rowIndex)
+    {
+        return strcasecmp(columnIndex.c_str(), other.columnIndex.c_str()) < 0;
+    }
+
+    return rowIndex < other.rowIndex;
+}
+
+std::ostream& operator<<(std::ostream& os, const CellPosition& pos)
+{
+    os << "Column: " << pos.columnIndex << " Row: " << pos.rowIndex << '\n';
+    return os;
+}
+
+//------------------------------------------------------------------------------

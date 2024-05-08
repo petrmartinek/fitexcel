a1b079b116da90dafa3294e94257c74e293eaad7
Petr Martínek <martip16@fit.cvut.cz>
2024-05-09 00:16:04 +0200

    hwBIG: add number representation for CellPosition

--- hwBIG/include/CellPosition.hpp
+++ hwBIG/include/CellPosition.hpp
@@ -7,18 +7,33 @@
 
 struct CellPosition
 {
-    explicit CellPosition(const std::string_view& str);
-    
+    explicit CellPosition(const std::string_view& str)
+        : identifier(str), columnString(), columnIndex(), rowIndex()
+    {}
+    CellPosition(size_t column, size_t row)
+        : identifier(), columnString(), columnIndex(column), rowIndex(row)
+    {}
+
     bool operator<(const CellPosition& other) const;
 
+    const std::string& string() { return createIdentifier(); }
     size_t row() const { return rowIndex; }
-    const std::string& column() const { return columnIndex; }
+    size_t column() const { return columnIndex; }
 
     friend std::ostream& operator<<(std::ostream& os, const CellPosition& pos);
 
 private:
+    const std::string& createIdentifier();
+
+    static std::string decToBase26(size_t decimal);
+    static size_t base26ToDec(const std::string& hexavigesimal);
+                                    // base26 == hexavigesimal https://en.wiktionary.org/wiki/hexavigesimal
+
+    std::string identifier;
+    std::string columnString;
+
+    size_t columnIndex;
     size_t rowIndex;
-    std::string columnIndex; // maybe not a good idea? consider numbers if issues arise
 };
 
 using CPos = CellPosition; // for progtest
--- hwBIG/src/CellPosition.cpp
+++ hwBIG/src/CellPosition.cpp
@@ -8,6 +8,8 @@
 #include <cctype>
 #include <stdexcept>
 #include <algorithm>
+#include <cmath>
+#include <cctype>
 
 CellPosition::CellPosition(const std::string_view& str)
 {
@@ -23,7 +25,9 @@ CellPosition::CellPosition(const std::string_view& str)
         ++position;
     }
 
-    columnIndex = str.substr(0, position);
+    columnString = str.substr(0, position);
+
+    columnIndex = base26ToDec(columnString);
 
     std::istringstream iss(std::string{str.substr(position)});
 
@@ -39,7 +43,7 @@ bool CellPosition::operator<(const CellPosition& other) const
 {
     if(rowIndex == other.rowIndex)
     {
-        return strcasecmp(columnIndex.c_str(), other.columnIndex.c_str()) < 0;
+        return columnIndex < other.columnIndex;
     }
 
     return rowIndex < other.rowIndex;
@@ -52,3 +56,67 @@ std::ostream& operator<<(std::ostream& os, const CellPosition& pos)
 }
 
 //------------------------------------------------------------------------------
+
+// ♥♥♥ all love goes to this guy https://www.mathsisfun.com/base-conversion-method.html ♥♥♥
+// this helped me how to make base-10 => base-26 and change the other way round
+
+// base-26:
+// Z = 0, A = 1, B = 2, ... , Y = 25
+
+constexpr int RADIX = 26;
+constexpr int UPPER_ALPHABET_INDEX_MOD = 'A' - 1;
+constexpr int LOWER_ALPHABET_INDEX_MOD = 'a' - 1;
+
+std::string CellPosition::decToBase26(size_t decimal)
+{
+    std::string hexavigesimal;
+
+    while(decimal)
+    {
+        int remainder = decimal % RADIX;
+
+        hexavigesimal.push_back(remainder != 0 ? UPPER_ALPHABET_INDEX_MOD + remainder : 'Z');
+
+        decimal /= RADIX;
+    }
+
+    std::reverse(hexavigesimal.begin(), hexavigesimal.end());
+
+    return hexavigesimal;
+}
+
+size_t CellPosition::base26ToDec(const std::string& hexavigesimal)
+{
+    size_t decimal = 0;
+    size_t index = 0;
+
+    for(auto it = hexavigesimal.rbegin(); it != hexavigesimal.rend(); ++it)
+    {
+        char coefficient = *it % (std::isupper(*it) ? UPPER_ALPHABET_INDEX_MOD : LOWER_ALPHABET_INDEX_MOD) % RADIX;
+        
+        decimal += coefficient * std::pow(RADIX, index);
+
+        ++index;
+    }
+
+    return decimal;
+}
+
+//------------------------------------------------------------------------------
+
+const std::string& CellPosition::createIdentifier()
+{
+    if(!identifier.empty())
+    {
+        return identifier;
+    }
+
+    if(columnString.empty())
+    {
+        columnString = decToBase26(columnIndex);
+    }
+
+    identifier = columnString + std::to_string(rowIndex);
+
+    return identifier;
+}

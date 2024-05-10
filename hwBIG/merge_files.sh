689af318e09e5a4571b05233a8f0d99ea869f840
Petr Martínek <martip16@fit.cvut.cz>
2024-05-10 22:36:00 +0200

    hwBIG: fix

--- hwBIG/merge_files.sh
+++ hwBIG/merge_files.sh
@@ -3,4 +3,4 @@ grep -vh '^#include' \
     include/CellValue.hpp include/CellPosition.hpp include/AST.hpp include/ExpressionBuilder.hpp include/Spreadsheet.hpp \
     src/CellPosition.cpp src/AST.cpp src/ExpressionBuilder.cpp src/Spreadsheet.cpp \
     > almost
-grep -vh '^using namespace' almost > all_in_one; rm almost
+grep -vh '^using namespace' almost > all_in_one.cpp; rm almost
